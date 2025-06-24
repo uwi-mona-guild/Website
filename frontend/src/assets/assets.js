838a73f6171abeabafe2723fb2c960f695461b39
TiannaBassaragh <tianna.bassaragh@outlook.com>
2025-06-24 04:27:26 -0500

    Updated files after conflict resolution merge

--- frontend/src/assets/assets.js
+++
@@ -1,6 +0,0 @@
-import uwi_logo from './uwi_logo.png';
-
-
-export const assets = {
-  uwi_logo,
-};
---
+++ frontend/src/assets/index.js
@@ -0,0 +1,2 @@
+// src/components/Assets/index.js
+export { default as banner } from './uwi-mona-guild-banner.png';
