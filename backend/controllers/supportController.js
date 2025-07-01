061cbf054f0d5c70a28f93dbe3967a0d778b4760
Jojo5673 <judoudo2005@gmail.com>
2025-06-30 20:56:04 -0500

    Added used packages to package.json

--- backend/controllers/supportController.js
+++ backend/controllers/supportController.js
@@ -1,5 +1,9 @@
 // Support/Assistance Controller
-exports.getAllSupportRequests = async (req, res) => {
+getAllSupportRequests = async (req, res) => {
   // Placeholder: Fetch all support requests
   res.json([{ id: 1, type: "complaint", message: "Example support request" }]);
 };
+
+module.exports = {
+  getAllSupportRequests,
+}
