9b2ee020e058a3e88e33bafaa964211a38cc1473
Joseph Udoudo <139509320+Jojo5673@users.noreply.github.com>
2025-07-07 00:59:45 -0500

    Mock data functionality and Templates for models, routes, and controllers (#22)
    * Mock data functionality and Templates for models, routes, and controllers
    
    * Changed date to hold both date and time
    
    * Update template.js to have correct naming convention
    
    * Update event.js to have a default cost
    
    * Fixed more things naming convention-wise
    
    ---------
    
    Co-authored-by: TiannaBassaragh <tianna.bassaragh@outlook.com>
    Co-authored-by: Tianna <151879019+TiannaBassaragh@users.noreply.github.com>

--- backend/models/eventsModel.js
+++ backend/models/eventsModel.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 const eventSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  date: { type: Date, required: true },
-  location: { type: String, required: true },
+  date: { type: Date, required: true }, // Will include date and time (if a time is given)
+  location: { type: String, required: true, default: "TBA" },
   description: { type: String },
   price: {
-    amount: { type: Number }, // Optional, event can be free if not set
+    amount: { type: Number, default: 0 }, // Optional, event can be free if not set
     currency: { type: String, enum: ['JMD', 'USD'], default: 'JMD' } // Optional, defaults to JMD
   },
   createdAt: { type: Date, default: Date.now }
