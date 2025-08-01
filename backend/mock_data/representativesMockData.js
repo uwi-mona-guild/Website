fd32844fd86a8ac9208c942b1fd6d3e9635dc30c
Jojo5673 <judoudo2005@gmail.com>
2025-08-01 07:24:13 -0500

    Added mock data for reps

---
+++ backend/mock_data/representativesMockData.js
@@ -0,0 +1,40 @@
+const regular_data = [
+  {
+    name: "Alice Smith",
+    role: "President",
+    description: "Loves food",
+    year: 2025,
+    major: "Computer Science",
+    photoUrl: "https://example.com/photos/alice.jpg",
+    socials: [
+      "https://github.com/alicesmith",
+      "https://linkedin.com/in/alicesmith"
+    ]
+  },
+  {
+    name: "Bob Johnson",
+    role: "Vice President",
+    description: "Loves sleep",
+    year: 2023,
+    major: "Mechanical Engineering",
+    photoUrl: "https://example.com/photos/bob.jpg",
+  },
+  {
+    name: "Chloe Zhang",
+    role: "Treasurer",
+    description: "Loves money",
+    year: 2022,
+    major: "Finance",
+    photoUrl: "https://example.com/photos/chloe.jpg",
+    socials: [
+      "https://linkedin.com/in/chloezhang",
+      "https://github.com/chloezhangml",
+      "https://medium.com/@chloe.ai"
+    ]
+  }
+]
+
+module.exports = {
+	regular_data,
+};
+
--- backend/models/representativeModel.js
+++ backend/models/representativeModel.js
@@ -3,9 +3,11 @@ const mongoose = require('mongoose');
 const representativeSchema = new mongoose.Schema({
   name: { type: String, required: true },
   role: { type: String, required: true },
-  email: { type: String },
-  phone: { type: String },
+  year: {type: Number, required: true},
+  description: { type: String },
   photoUrl: { type: String },
+  socials: [{ type: String }], 
+  major: { type: String },
   createdAt: { type: Date, default: Date.now }
 });
 
