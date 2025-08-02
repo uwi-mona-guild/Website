564ed3a360920e195d7aecba915d55dc43999983
Jojo5673 <judoudo2005@gmail.com>
2025-08-01 19:32:39 -0500

    Edited data loading to remove loadenv package

--- backend/mock_data/representativesMockData.js
+++ backend/mock_data/representativesMockData.js
@@ -5,7 +5,7 @@ const regular_data = [
     description: "Loves food",
     year: 2025,
     major: "Computer Science",
-    photoUrl: "https://example.com/photos/alice.jpg",
+    photoUrl: "https://media.istockphoto.com/id/1386479313/photo/happy-millennial-afro-american-business-woman-posing-isolated-on-white.jpg?s=612x612&w=0&k=20&c=8ssXDNTp1XAPan8Bg6mJRwG7EXHshFO5o0v9SIj96nY=",
     socials: [
       "https://github.com/alicesmith",
       "https://linkedin.com/in/alicesmith"
@@ -17,7 +17,7 @@ const regular_data = [
     description: "Loves sleep",
     year: 2023,
     major: "Mechanical Engineering",
-    photoUrl: "https://example.com/photos/bob.jpg",
+    photoUrl: "https://thumbs.dreamstime.com/b/headshot-african-american-male-glasses-posing-studio-portrait-smiling-man-wearing-casual-clothes-look-camera-image-153874435.jpg",
   },
   {
     name: "Chloe Zhang",
@@ -25,7 +25,7 @@ const regular_data = [
     description: "Loves money",
     year: 2022,
     major: "Finance",
-    photoUrl: "https://example.com/photos/chloe.jpg",
+    photoUrl: "https://media.istockphoto.com/id/1394347360/photo/confident-young-black-businesswoman-standing-at-a-window-in-an-office-alone.jpg?s=612x612&w=0&k=20&c=tOFptpFTIaBZ8LjQ1NiPrjKXku9AtERuWHOElfBMBvY=",
     socials: [
       "https://linkedin.com/in/chloezhang",
       "https://github.com/chloezhangml",
